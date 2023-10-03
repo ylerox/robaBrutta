e190ac62f1e2180d8f77bbd2247e181cfb3770ee
Ylenia <ylerox@hotmail.com>
2023-10-03 15:09:15 +0200

    aggiornato nextConfig e aggiunto build steps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  output: "export",
+  basePath: "robaBrutta",
+  images: {
+  unoptimized: true,
+  }
+};
 
 module.exports = nextConfig
